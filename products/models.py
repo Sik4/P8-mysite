cd178dcd34e7d0038ead66cf08351a5292cfde8e
Sik4 <cam.maury@gmail.com>
2019-04-30 07:58:28 +0200

    products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Products(models.Model):
+    products_title = models.CharField(max_length=200)
+    products_content = models.TextField()
+    products_published = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.products_title
